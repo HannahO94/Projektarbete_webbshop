ca1bce1808151ab402390d24be39f64154471d34
anra1901 <54841548+anra1901@users.noreply.github.com>
2020-04-12 11:49:14 +0200

    Skapat mapp för produktsida samt gjort HTML för den

---
+++ product/product-info.php
@@ -0,0 +1,25 @@
+<h1>Spelnamn</h1>
+
+<div class="img-container">
+    <img src="" alt="Bild på spelet" class="img-container__img">
+</div>
+
+<h2>Beskrivning</h2>
+
+<p class="description">
+Bitters fixie 8-bit, offal man braid palo santo thundercats butcher vapor ware 
+fanny pack vegan pinterest mixtape retro twee. Air plant cloud bread narwhal 
+ramps sriracha hoodie celiac DIY. Food truck adaptogen fam, microdosing letterpress 
+hell of slow-carb. Fashion axe tilde try-hard you probably haven't heard of 
+them mixtape microdosing poke knausgaard shaman seitan quinoa chicharrones drinking 
+vinegar butcher. 90's whatever chambray blog man braid craft beer jean shorts 
+dreamcatcher cray.
+</p>
+
+<span>Pris: 500 kr</span>
+
+<?php 
+// Finns i lager - Finns EJ i lager
+?>
+
+<button class="cart-btn">Lägg i varukorgen</button>
