1b137e4299029ffb344d4e110e0306da38a93341
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-04-13 17:18:02 +0200

    Update db.php
    Säkerhetstillägg: Förhindra PDO att använda simulerade förfrågningar

--- config/db.php
+++ config/db.php
@@ -5,18 +5,26 @@
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
 
+
 //Uppgifter om databasen
 $db_server = "localhost"; //mysql:host (se new PDO i try-catch-satsen) är nästan alltid localhost (90% av fallen)
 $db_database = "webshop";
 $db_username = "root";
-$db_password = ""; // password för XAMPP = ""
-//$db_password = "root" //password för MAMP = "root"
+$db_password = ""; //password för XAMPP
+//$db_password = "root" //password för MAMP
 
-//Skapar ny uppkoppling mot databasen, eller visar felmeddelande vid ev. fel
+
+//Skapar ny uppkoppling mot databasen
 try {
   $db = new PDO("mysql:host=$db_server;dbname=$db_database;charset=utf8", $db_username, $db_password);
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //PDO är ett objekt. 
-  echo "<h2>Connected successfully</h2>";
+
+  //Aktiverar hantering av felmeddelanden
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  //Förhindrar PDO att använda simulerade förfrågningar
+  $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+  //Visar felmeddelande om uppkopplingen misslyckas
 } catch (PDOException $e) {
   echo "<h2>Error: " . $e->getMessage() . "</h2>";
 }
