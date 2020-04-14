97c2b48389e7fcae0a715c68aa8945343748ee7d
Annfo637 <54262041+Annfo637@users.noreply.github.com>
2020-04-14 15:40:02 +0200

    Kategorisida påbörjad
    Grundläggande html, css och php för frontpage och categorypage. Kvarstår länkar bl a.

---
+++ categorypage/categorypage.php
@@ -0,0 +1,42 @@
+<?php
+require_once '../config/db.php';
+
+if($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $currentCategory = htmlspecialchars($_GET['categoryid']);
+  }
+
+//måste även lägga till ett WHERE-villkor som matchar den aktuella kategorin, variabeln ovan
+$stmt = $db->prepare("SELECT title FROM products WHERE categoryid = $currentCategory");
+$stmt->execute();
+
+?>
+
+<!--hero-sektion och sektion för produkter--> 
+<section class="hero">
+    <div class="shoppingcart">
+    </div>
+    <div class="categorypage_logo img-container">
+    <img class="img-container__img" src="category.jpg" alt="Kategoribild">
+    </div>
+ </section>
+ <section>
+  <h1 class="category_name">Kategorinamn</h1>
+  <br>
+  <br>
+ <!--här hämtas kategoriens produkter från databas-->
+  <div class="categorypage_products">
+  <?php
+  while($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
+          $title = htmlspecialchars($row['title']);
+          
+          echo 
+          "<div class='product_card'>
+                  <h2 class='product_title'>$title</h2>
+          </div>";
+    
+      endwhile;
+  ?>
+  </div>
+ </section>  
+
+
---
+++ categorypage/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once '../header_extern.php';
+require_once 'categorypage.php';
+require_once '../footer_extern.php';
