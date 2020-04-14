a7be385f542b31a2fb652d208d40090791c1158c
madeleineenberg <madeleine_enberg@hotmail.com>
2020-04-14 15:08:25 +0200

    lägga till produkter

--- admin/admin-add-products.php
+++ admin/admin-add-products.php
@@ -1,24 +1,67 @@
 <?php
 
 require_once 'header.php';
-require_once 'db.php';
+require_once '../config/db.php';
+
+
+
+$msg = "";
+
+        $sql = "SELECT * FROM categories";
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+
+        $option_value = "";
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $categoryid = htmlspecialchars($row['categoryid']);
+            $category = htmlspecialchars($row['category']);
+            $option_value .= "<option value='$categoryid'>$category</option>";
+        }
+
+
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') :
+        
+        $sql = "INSERT INTO products (title, description, categoryid, price)
+                VALUES (:title, :description, :categoryid, :price) ";
+           
+        $stmt = $db->prepare($sql);
+
+        
+        $title = htmlspecialchars($_POST['title']);
+        $description = htmlspecialchars($_POST['description']);
+        $price = htmlspecialchars($_POST['price']);
+        $categoryid = $_POST['category'];
+
+        // $image = $_FILES['image']['name'];
+        // $link = $_POST['link'];
+        // $target = "../images/".basename($image);
+        
+
+        
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':price', $price);
+        $stmt->bindParam(':categoryid', $categoryid);
+
+
+        // $stmt->bindParam(':image', $image);
+        // $stmt->bindParam(':link', $link);
+        // $stmt->bindParam(':published', $published);
+       
+       
+        $stmt->execute(); 
+
+        
+        // if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+        //     $msg = "Bilden är uppladdad!";
+        // }else{
+        //     $msg = "Ingen bild är uppladdad!";
+        // }
+    endif;
+
 
 ?>
-
-
-<div>Admin</div>
-<nav><ul>
-    <!-- Ska sätta css display none i den inre listan som togglas men hjälp av javascript? -->
-        <li>ADMIN
-            <ul id="hide-sub-list" style="display: none;"> 
-                <li>Kategorier</li>
-                <li>Produkter</li>
-                <li>Beställningar</li>
-            </ul>
-        </li>
-        <li>WEBBSHOPPEN</li>
-    </ul>
-</nav>
 
 <section class="page-title-container">
 
@@ -31,8 +74,8 @@ require_once 'db.php';
 <form action="#" method="POST" enctype="multipart/form-data">
 
 <div class="product_field-name">
-<label for="product">Produkt namn: </label><br>
-<input type="text" name="product">
+<label for="title">Produkt namn: </label><br>
+<input type="text" name="title">
 </div>
 
 <div class="product_field-price">
@@ -40,13 +83,17 @@ require_once 'db.php';
 <input type="text" name="price">
 </div>
 
+<div class="product_field-instock">
+<label for="instock">Ange lagerstatus: <br>
+<input type="number" min="0" max="500" name="instock">
+</div>
+
 <div class="product_field-category">
 <label for="category">Kategori: </label><br>
-<select id="category">
+<select name="category">
 <option value="">Välj en kategori...</option>
-<option value=""></option>
-<option value=""></option>
-<option value=""></option>
+<?php echo $option_value; ?>
+
 </select>
 </div>
 
@@ -56,8 +103,8 @@ require_once 'db.php';
 </div>
 
 <div class="product_field-description">
-<label for="product-description">Beskrivning: </label><br>
-<textarea name="product-description" placeholder="Beskrivning av produkt.." cols="10" rows="8"></textarea>
+<label for="description">Beskrivning: </label><br>
+<textarea name="description" placeholder="Beskrivning av produkt.." cols="10" rows="8"></textarea>
 </div>
 
 <div class="product_field-submit">
