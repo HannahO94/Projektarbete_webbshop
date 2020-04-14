18cd28fdfc77a4cab5af0d397a646429450119cc
anra1901 <54841548+anra1901@users.noreply.github.com>
2020-04-14 10:52:58 +0200

    Ändringar på filnamnen

--- product/index.php
+++ product/index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once '../header.php';
-require_once 'product-info.php';
-require_once '../footer.php';
+require_once '../header_extern.php';
+require_once 'product_info.php';
+require_once '../footer_extern.php';
 
 ?>
