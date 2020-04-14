ae05dd53ebc3297bd02f1dadb3953333d8d131d9
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-04-14 16:47:35 +0200

    Ändrat tabellnamnen i enlighet med databasen (lagt till prefixet "webshop_" framför)

--- frontpage.php
+++ frontpage.php
@@ -1,30 +1,30 @@
 <?php
 // Denna sida hämtar och ritar ut aktuella kategorier från databasen
-    require_once 'config/db.php';
-    $stmt = $db->prepare("SELECT category FROM categories");
-    $stmt->execute();
+require_once 'config/db.php';
+$stmt = $db->prepare("SELECT category FROM webshop_categories");
+$stmt->execute();
 
-?>    
+?>
 
-<!--detta ska vara skalet för en startsida, förstasida för webbshoppen--> 
+<!--detta ska vara skalet för en startsida, förstasida för webbshoppen-->
 <section class="hero">
     <div class="shoppingcart">
     </div>
     <div class="frontpage_logo img-container">
-    <img class="img-container__img" src="logo.jpg" alt="Logo Spelshoppen">
+        <img class="img-container__img" src="logo.jpg" alt="Logo Spelshoppen">
     </div>
- </section>
- <section class="frontpage_categories">
- <!--här hämtas kategorier från databas-->
- <?php
- while($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
+</section>
+<section class="frontpage_categories">
+    <!--här hämtas kategorier från databas-->
+    <?php
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
         $category = htmlspecialchars($row['category']);
-        
-        echo 
-        "<div class='category_card'>
+
+        echo
+            "<div class='category_card'>
                 <h2 class='category_title'>$category</h2>
         </div>";
-   
+
     endwhile;
-?>
- </section>  
+    ?>
+</section>
