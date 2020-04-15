dc5b911da244e345e61e69f69e38e98130cc5619
anra1901 <54841548+anra1901@users.noreply.github.com>
2020-04-15 15:11:43 +0200

    Navigeringsmeny utan styling

--- header_extern.php
+++ header_extern.php
@@ -8,3 +8,34 @@
     <link rel="stylesheet" type="text/css" href="styles/main.css" />
     <title>Spelshoppen</title>
 </head> 
+
+<body>
+
+<header>
+
+<div class="menu-wraper">
+      <div class="menu-wraper__logo-wrap">
+        <img src="" alt="Logo" />
+      </div>
+      <nav>
+        <ul class="menu-wraper__link-list">
+          <li>
+            <a class="menu-wraper__links--active" href="../index.php">HEM</a>
+          </li>
+          <li>
+            <a class="menu-wraper__links" href="../index.php">KATEGORIER</a>
+          </li>
+          <li>
+            <a class="menu-wraper__links" href="">KONTAKT</a>
+          </li>
+          <li>
+            <a class="menu-wraper__links" href="/search/index.php">SÖK</a>
+          </li>
+          <li>
+            <a class="menu-wraper__links" href="/admin/index.php">ADMIN</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    
+    </header>
