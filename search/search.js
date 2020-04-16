9360037535a572680fb2de81b3df08df3a6ad9fe
MimmiSethfors <mimmisethfors@gmail.com>
2020-04-16 17:45:26 +0200

    search
    Ditt sökresultat är kopplat till produktsidan med en länk och id.
    Går inte att söka på beskrivning än.
    Bugg:
    dubbletter kommer upp när man skriver in hela ordet.

--- search/search.js
+++ search/search.js
@@ -16,11 +16,14 @@ ajax.onreadystatechange = function () {
     let games = JSON.parse(this.responseText);
 
     let gamesTitle = new Array;
+                                    //let gamesDescription = new Array;
 
     for (let i = 0; i < games.length; i++) {
       //console.log(games[i].title)
-      gamesTitle.push(games[i].title) 
+      gamesTitle.push(games[i].title)
+                                       //gamesDescription.push(games[i].description)
     }
+    //console.log(gamesDescription)
     //console.log(gamesTitle)
     //eventlistener på sökfältt
     searchField.addEventListener('input', function (event) {
@@ -36,19 +39,26 @@ ajax.onreadystatechange = function () {
       let searchedGame = gamesTitle.filter(function (game) {
         return game.toLowerCase().includes(searchField.value.toLowerCase())
       })
+                                              /*let searchedDescription = gamesDescription.filter(function (descr) {
+                                                return descr.toLowerCase().includes(searchField.value.toLowerCase())
+                                              })*/
+
       //console.log(searchedGame)
+      //console.log(searchedDescription)
 
       for (let i = 0; i < games.length; i++) {
         //console.log(games[i].title)
         for (let j = 0; j < searchedGame.length; j++) {
-          if (games[i].title === searchedGame[j]) {
-            console.log(games[i].title)
-            console.log(games[i].price)
-            let listedGames = document.createElement('p')
-            listedGames.textContent = games[i].title + " " + games[i].price;
+
+          if (games[i].title === searchedGame[j] ) {        //|| games[i].description === searchedDescription[j]
+            //console.log(searchedDescription[j])
+            //console.log(games[i].description)
+            //console.log(games[i].title)
+            //console.log(games[i].price)
+            let listedGames = document.createElement('a')
+            listedGames.textContent = games[i].title + " " + games[i].price + " kr ";
+            listedGames.href = "../product/product_info.php?id= " + games[i].productid;
             searchedGames.appendChild(listedGames);
-
-
           }
           //console.log(searchedGame[j])
         }
