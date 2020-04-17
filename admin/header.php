8be3c3ced3da1369ed29eea066f98fe957877fab
anra1901 <54841548+anra1901@users.noreply.github.com>
2020-04-17 16:24:46 +0200

    Lagt till vänstermeny i admin med toggle

--- admin/header.php
+++ admin/header.php
@@ -3,20 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <link rel="stylesheet" href="/styles/main.css">
+    <title>Adminpanelen</title>
 </head>
-<body>
-<div class="admin-top">Admin</div>
-<nav><ul>
-    <!-- Ska sätta css display none i den inre listan som togglas men hjälp av javascript? -->
-        <li><a href="admin-home.php">ADMIN<a></li>
 
-            <ul> 
-                <li><a href="admin-category.php">Kategorier</a></li>
-                <li><a href="admin-products.php">Produkter</a></li>
-                <li><a href="">Beställningar</a></li>
+<body>
+
+    <button id="toggle-menu">Meny</button>
+ 
+    <div id="admin-menu-wraper" class="hide">
+        <nav>
+            <ul class="admin-menu-wraper__link-list"> 
+                <li class="admin-menu-wraper__link"><a href="index.php">Admin - Hem</a></li>
+                <li class="admin-menu-wraper__link"><a href="admin-category.php">Kategorier</a></li>
+                <li class="admin-menu-wraper__link"><a href="admin-products.php">Produkter</a></li>
+                <li class="admin-menu-wraper__link"><a href="">Beställningar</a></li>
+                <li class="admin-menu-wraper__link"><a href="../index.php">WEBBSHOPPEN</a></li>
             </ul>
-        </li>
-        <li><a href="../index.php">WEBBSHOPPEN</a></li>
-    </ul>
-</nav>
+        </nav>
+    </div>
+
+<script>
+
+let toggleMenuBtn = document.querySelector("#toggle-menu");
+let menu = document.querySelector("#admin-menu-wraper");
+
+toggleMenuBtn.addEventListener('click', function(event) {
+  if (menu.classList.contains('hide')) {
+    menu.classList.remove('hide')
+    toggleMenuBtn.textContent = 'Göm meny'
+  } else {
+    menu.classList.add('hide')
+    toggleMenuBtn.textContent = 'Meny'
+  }
+})
+
+</script>
