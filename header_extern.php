dc851ab97127a37c0d1c14515f26191523c55f96
anra1901 <54841548+anra1901@users.noreply.github.com>
2020-04-20 16:54:12 +0200

    Styling på adminsidan

--- header_extern.php
+++ header_extern.php
@@ -20,7 +20,6 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=
     , initial-scale=1.0">
-  <!--här gissar jag att CSS för webb - extern ska hamna-->
   <link rel="stylesheet" type="text/css" href="styles/main.css" />
   <link rel="stylesheet" type="text/css" href="../styles/main.css" />
   <title>Spelshoppen</title>
