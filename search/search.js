2df6a68c3669a13a1e7aa283a4d92de0c02f67d8
Annfo637 <54262041+Annfo637@users.noreply.github.com>
2020-04-22 19:20:33 +0200

    Produktkort på search/index

--- search/search.js
+++ search/search.js
@@ -1,9 +1,9 @@
 let searchResult = document.querySelector("#searched-result");
 let searchField = document.querySelector("#search-Field");
-let searchBtn = document.querySelector("#search_btn")
-let searchLink = document.querySelector("#search-link")
-let output = new Array;
-let productId = new Array;
+let searchBtn = document.querySelector("#search_btn");
+let searchLink = document.querySelector("#search-link");
+let output = new Array();
+let productId = new Array();
 
 // anropa ajax
 let ajax = new XMLHttpRequest();
@@ -15,30 +15,27 @@ ajax.send();
 //ta emot svar från php fil
 ajax.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
-
     //konvertera JSON tillbaka till array
     let games = JSON.parse(this.responseText);
-    let gamesTitle = new Array;
+    let gamesTitle = new Array();
 
     for (let i = 0; i < games.length; i++) {
       gamesTitle.push(games[i].title);
-      gamesTitle.push(games[i].description)
+      gamesTitle.push(games[i].description);
     }
 
     //eventlistener på sökknapp
     searchBtn.addEventListener("click", function (event) {
       //console.log("click")
-      searchLink.href = "../search/index.php?id=" + productId
+      searchLink.href = "../search/index.php?id=" + productId;
     });
     //eventlistener på sökfältt
     searchField.addEventListener("input", function (event) {
-      emptySearch()
+      emptySearch();
 
       //minst två tecken validering
       if (searchField.value.length >= 2) {
-
         filter();
-
       } else {
         searchResult.innerHTML = null;
       }
@@ -50,42 +47,44 @@ ajax.onreadystatechange = function () {
       let searchedGame = gamesTitle.filter(function (game) {
         return game.toLowerCase().includes(searchField.value.toLowerCase());
       });
-      console.log(searchedGame)
+      console.log(searchedGame);
       //töm båda arrayerna varje gång tanget trycks, annars ritas inte förfinade sökningen om
-      output.splice(0, output.length)
-      productId.splice(0, productId.length)
+      output.splice(0, output.length);
+      productId.splice(0, productId.length);
 
       for (let i = 0; i < games.length; i++) {
         for (let j = 0; j < searchedGame.length; j++) {
-          if (games[i].title === searchedGame[j] || games[i].description === searchedGame[j]) {
+          if (
+            games[i].title === searchedGame[j] ||
+            games[i].description === searchedGame[j]
+          ) {
             if (productId.includes(games[i].productid)) {
               display(productId, output);
             } else {
-              output.push(" | " + games[i].title + " " + games[i].price + " kr " + " | ")
-              productId.push(games[i].productid)
+              output.push(
+                " | " + games[i].title + " " + games[i].price + " kr " + " | "
+              );
+              productId.push(games[i].productid);
               display(productId, output);
-
             }
           }
         }
-
       }
-
     }
 
     function display() {
       //töm div innerhtml för att kunna rita om när en förfinad sökning görs
-      searchResult.innerHTML = " "
+      searchResult.innerHTML = " ";
 
       for (let i = 0; i < output.length; i++) {
         let listedGames = document.createElement("a");
         //console.log("output i forloop i display()  " + output + productId)
-        listedGames.textContent = output[i]
+        listedGames.textContent = output[i];
         listedGames.href = "../product/product_info.php?id= " + productId[i];
-        listedGames.id = productId[i]
+        listedGames.id = productId[i];
 
         //kontroll av id på a-tagg - om den finns rita inte ut en ny
-        let element = document.getElementById(productId[i])
+        let element = document.getElementById(productId[i]);
         if (element !== null) {
         } else {
           searchResult.appendChild(listedGames);
@@ -96,10 +95,8 @@ ajax.onreadystatechange = function () {
 
   function emptySearch() {
     if (searchField.value === "") {
-      output.splice(0, output.length)
-      productId.splice(0, productId.length)
-
+      output.splice(0, output.length);
+      productId.splice(0, productId.length);
     }
   }
-}
-
+};
