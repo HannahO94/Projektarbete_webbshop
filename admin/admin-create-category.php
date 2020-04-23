438ade64b6b716b21e37b8fabb257f1af877e6e4
HannahO94 <56817756+HannahO94@users.noreply.github.com>
2020-04-23 10:45:21 +0200

    Inputfältet valideras

--- admin/admin-create-category.php
+++ admin/admin-create-category.php
@@ -3,8 +3,10 @@ require_once '../config/db.php';
 require_once 'header.php';
 
 $nameErr = "";
+$msg = "";
+$errors = "";
+
 if($_SERVER['REQUEST_METHOD'] === 'POST') :
-    $error = array();
     $uploadOk = 1;
     $error = array();
     $imageFileType = strtolower(pathinfo($target,PATHINFO_EXTENSION));
@@ -14,53 +16,52 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') :
             echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
         } else {
-            // $error = "File is not an image.";
+            // $error[] = "File is not an image.";
             $uploadOk = 0;
         }
     }
     // Check if file already exists
     if (file_exists($target)) {
-        // $error = "Sorry, file already exists.";
+        // $error[] = "Sorry, file already exists.";
         $uploadOk = 0;
         $image = "";
     }
     // Check file size
     if ($_FILES["category-img"]["size"] > 500000) {
-        // $error = "Sorry, your file is too large.";
+        // $error[] = "Sorry, your file is too large.";
         $uploadOk = 0;
         $image = "";
     }
     // Allow certain file formats
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif" ) {
-        // $error = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        // $error[] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
         $uploadOk = 0;
         $image = "";
     }
     // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Filen laddades inte upp.";
-        $image = "";
-    // if everything is ok, try to upload file
-    } 
+    // if ($uploadOk == 0) {
+    //     $msg = "Ingen fil laddades upp.";
+    //     $image = "";
+    // // if everything is ok, try to upload file
+    // } 
     else {
-        if (move_uploaded_file($_FILES["category-img"]["tmp_name"], $target) && $uploadOk == true) {
-            echo "The file ". basename( $_FILES["category-img"]["name"]). " has been uploaded.";
+        if (move_uploaded_file($_FILES["category-img"]["tmp_name"], $target)) {
+            // echo "The file ". basename( $_FILES["category-img"]["name"]). " has been uploaded.";
             $image = $_FILES['category-img']['name'];
             $target = "../images/".basename($image);
+            
         } else {
             echo "Sorry, there was an error uploading your file.";
             
         }
     }
- 
-
+   
+    
     if (empty($_POST["category"])) {
         $nameErr = "Kategorinamn måste fyllas i";
       }
-    // if(count($errors) > 0)  {
-        
-    //   }
+  
     else if (!empty($_POST["category"])) {
         $category = test_input($_POST["category"]);
         // check if name only contains letters and whitespace
@@ -75,12 +76,20 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') :
             $stmt->bindParam(':category', $category);
             $stmt->bindParam(':image', $image);
             $stmt->execute(); 
-            header('Location:admin-category.php');
+            // header('Location:admin-category.php');
 
         }
     }
 
         // $category = htmlspecialchars($_POST['category']);
+        // if(count($error) > 0 && $image != ""){        
+        //     foreach($error as $e){
+        //         $errors .= "<div class='error'><p> $e </p></div><br />";      
+        //     }
+        // }   
+        // else{
+        //         $errors = "<div class='suc'><p> Din Produkt är sparad!</p></div>";
+        // }
         
     endif;
     function test_input($data) {
@@ -89,6 +98,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') :
         $data = htmlspecialchars($data);
         return $data;
       }
+     
  
    
 ?>
@@ -107,6 +117,11 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') :
     <div class="category_field-img form-container__image">
         <label for="category-img">Ladda upp en katagoribild: </label><br>
         <input type="file" name="category-img" class="form-container__image-input">
+        <p class="error"><?php 
+        // echo $msg;
+        echo $errors;
+        ?></p>
+
     </div>
 
     <div class="category_field-submit form-container__submit">
