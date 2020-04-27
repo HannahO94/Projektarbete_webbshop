27d0c6b2786f428111f03a4d0413c21937f3b491
anra1901 <54841548+anra1901@users.noreply.github.com>
2020-04-27 13:38:38 +0200

    Lite mer validering inlagd

--- order/validate_order.js
+++ order/validate_order.js
@@ -1,6 +1,8 @@
 function validateForm(event) {
   event.preventDefault();
-  return validateName() && validateZipcode() && validateEmail();
+  return (
+    validateName() && validateZipcode() && validateEmail() && validatePhone()
+  );
 }
 
 // Validering av namn-fältet
@@ -10,24 +12,23 @@ function validateName() {
 
   if (name.length === 0) {
     infoText.innerHTML = "OBS! Obligatorisk fält";
-  } else if (!validate(name)) {
-    infoText.innerHTML = "OBS! Ogiltigt namn";
-  } else if (!isNaN(name)) {
-    infoText.innerHTML = "OBS! Endast text tillåtet";
+  } else if (new RegExp("[0-9]").test(name)) {
+    infoText.innerHTML = "OBS! Inga siffror tillåtna";
   } else if (name.length > 20) {
     infoText.innerHTML = "OBS! Otillåtet med fler än 20 tecken";
   } else if (name.length < 2) {
     infoText.innerHTML = "OBS! Måste skriva mer än 2 tecken";
+  } else if (isValidName(name)) {
+    infoText.innerHTML = "OBS! Ogiltigt namn";
   } else {
     infoText.innerHTML = "";
     return true;
   }
   return false;
 }
-
-function validate(name) {
-  let re = /^[A-Za-z\x{00C0}-\x{00FF}][A-Za-z\x{00C0}-\x{00FF}\'\-]+([\ A-Za-z\x{00C0}-\x{00FF}][A-Za-z\x{00C0}-\x{00FF}\'\-]+)*/u;
-  return re.test(name);
+function isValidName(name) {
+  let re = /[^a-öA-Ö\s:]/;
+  return re.test(String(name));
 }
 
 // Validering av mailadressen
@@ -37,17 +38,43 @@ function validateEmail() {
 
   if (email.length === 0) {
     infoText.innerHTML = "OBS! Obligatorisk fält";
-  } else if (!validate(email)) {
+  } else if (!isValidEmail(email)) {
     infoText.innerHTML = "OBS! Ogiltig epostadress";
   } else if (email.length > 64) {
     infoText.innerHTML = "OBS! Otillåtet med fler än 64 tecken";
   } else {
     infoText.innerHTML = "";
+    return true;
   }
+  return true;
 }
-function validate(email) {
+function isValidEmail(email) {
   let re = /^(([^<>()\[\]\\%.,;:\s@"]+(\.[^<>()\[\]\\%.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
+}
+
+// Validering av mobilnummer
+function validatePhone() {
+  let phone = document.querySelector("#phone").value;
+  let infoText = document.querySelector(".phoneValidationText");
+
+  if (phone.length === 0) {
+    infoText.innerHTML = "OBS! Obligatorisk fält";
+  } else if (new RegExp("[a-öA-Ö]").test(phone)) {
+    infoText.innerHTML = "OBS! Inga bokstäver tillåtna";
+  } else if (isValidPhone(phone)) {
+    infoText.innerHTML = "OBS! Ogiltigt mobilnummer";
+  } else if (phone.length != 10) {
+    infoText.innerHTML = "OBS! Numret måste vara 10 siffror långt";
+  } else {
+    infoText.innerHTML = "";
+    return true;
+  }
+  return false;
+}
+function isValidPhone(phone) {
+  let re = /[^0-9:]/;
+  return re.test(String(phone));
 }
 
 // Validering av postnummer-fältet
