4dc40f91808732ce136d177facf889aa13d7f9c6
anra1901 <54841548+anra1901@users.noreply.github.com>
2020-04-28 15:00:41 +0200

    Satte "required" på inputfälten

--- order/orderpage.php
+++ order/orderpage.php
@@ -63,7 +63,7 @@ require_once '../config/db.php';
     <div class="order_field-name form-container__box">
       <label for="name">För- och efternamn:</label><br>
       <!-- <input type="text" name="name" id="name" required> -->
-      <input type="text" name="name" id="name" onblur="validateName()" class="form-container__box-input">
+      <input type="text" name="name" id="name" onblur="validateName()" class="form-container__box-input" required>
       <br>
       <span class="nameValidationText"></span>
     </div>
@@ -71,21 +71,21 @@ require_once '../config/db.php';
     <div class="order_field-email form-container__box">
       <label for="email">E-post:</label><br>
       <!-- <input type="text" name="email" id="email" required> -->
-      <input type="text" name="email" id="email" onblur="validateEmail()" class="form-container__box-input" placeholder="exempel@test.com">
+      <input type="text" name="email" id="email" onblur="validateEmail()" class="form-container__box-input" placeholder="exempel@test.com" required>
       <br>
       <span class="emailValidationText"></span>
     </div>
 
     <div class="order_field-phone form-container__box">
       <label for="phone">Mobilnummer:</label><br>
-      <input type="text" name="phone" id="phone" onblur="validatePhone()" class="form-container__box-input" placeholder="(ex. 0701234567)">
+      <input type="text" name="phone" id="phone" onblur="validatePhone()" class="form-container__box-input" placeholder="(ex. 0701234567)" required>
       <br>
       <span class="phoneValidationText"></span>
     </div>
 
     <div class="order_field-street form-container__box">
       <label for="street">Gatuadress:</label><br>
-      <input type="text" name="street" id="street" onblur="validateStreet() " class="form-container__box-input">
+      <input type="text" name="street" id="street" onblur="validateStreet() " class="form-container__box-input" required>
       <br>
       <span class="streetValidationText"></span>
     </div>
@@ -93,14 +93,14 @@ require_once '../config/db.php';
     <div class="order_field-postalcode form-container__box">
       <label for="zip">Postnr:</label><br>
       <!-- <input type="number" name="zip" id="zip" required> -->
-      <input type="text" name="zip" id="zip" onblur="validateZipcode()" placeholder="(ex. 12345)" class="form-container__box-input">
+      <input type="text" name="zip" id="zip" onblur="validateZipcode()" placeholder="(ex. 12345)" class="form-container__box-input" required>
       <br>
       <span class="zipcodeValidationText"></span>
     </div>
 
     <div class="order_field-city form-container__box">
       <label for="city">Ort:</label><br>
-      <input type="text" name="city" id="city" onblur="validateCity()" class="form-container__box-input">
+      <input type="text" name="city" id="city" onblur="validateCity()" class="form-container__box-input" required>
       <br>
       <span class="cityValidationText"></span>
     </div>
